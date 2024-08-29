a36da219f227bd8b731b28cc582c5499411c632a
cgaswin <cgaswin333@gmail.com>
2024-08-29 21:56:55 +0530

    created eureka registry server

--- Event/src/main/java/org/event/event/model/Event.java
+++ Event/src/main/java/org/event/event/model/Event.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
---
+++ eureka-registry/src/test/java/org/registry/eurekaregistry/EurekaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.registry.eurekaregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
